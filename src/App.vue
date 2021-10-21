132353c5fd5d4a927049dc979f0ff2eb1c79810b
Marija Nikolcheva Saliji <mnikolceva@gmail.com>
2021-10-21 14:29:09 +0200

    GitHub action setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
  <div id="app">
-   <task-list />
+   <InboxScreen />
  </div>
 </template>
 
 <script>
-import TaskList from './components/TaskList.vue';
+import InboxScreen from './components/InboxScreen.vue';
 
 
 export default {
   name: "App",
   components: {
-    TaskList
+    InboxScreens
   }
 };
 </script>
